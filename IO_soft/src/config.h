766eb1da13524d64067ba58fe41e130e156aff91
ENAC-Robotique <robotique.enac@gmail.com>
2024-02-22 19:44:21 +0100

    config + tests demo ok

---
+++ IO_soft/src/config.h
@@ -0,0 +1,27 @@
+#define MAG1 PA11
+#define MAG3 PB8
+#define MAG2 PB9
+
+#define AX12A_PIN PC4
+#define AX_PINCE 7
+#define AX_POT 5
+#define AX_SPEED 300
+
+#define POT_UP 550
+#define POT_DOWN 210
+
+#define PINCE_UP 975
+#define PINCE_MID 700
+#define PINCE_DOWN 270
+
+#define SERVO1_PIN PA7
+#define SERVO1_OPEN 800
+#define SERVO1_CLOSED 1200
+
+#define SERVO2_PIN PA8
+#define SERVO2_OPEN 1600
+#define SERVO2_CLOSED 1950
+
+#define SERVO3_PIN PA9
+#define SERVO3_OPEN 1200
+#define SERVO3_CLOSED 1516
--- IO_soft/src/main.cpp
+++ IO_soft/src/main.cpp
@@ -1,48 +1,18 @@
 #include <Arduino.h>
-#include "../lib/metro.h"
+#include "metro.h"
 #include "AX12A.h"
 #include <Servo.h>
+#include "config.h"
 
-#define MAG3 PA11
-#define MAG2 PB8
-#define MAG1 PB9
 
-#define STEPPER_2_STP PC1
-#define STEPPER_2_DIR PC0
-
-#define AX12A_PIN PC4
-
-#define POT_UP 550
-#define POT_DOWN 210
-
-#define PINCE_UP 975
-#define PINCE_MID 570
-#define PINCE_DOWN 360
-
-#define SERVO1_PIN PA7
-#define SERVO1_OPEN 760
-#define SERVO1_CLOSED 1140
-
-#define SERVO2_PIN PA8
-#define SERVO2_OPEN 1500
-#define SERVO2_CLOSED 1800
-
-#define SERVO3_PIN PA9
-#define SERVO3_OPEN 1200
-#define SERVO3_CLOSED 1516
-
-Metro tic_tac(2000); // timer exécuté toutes les 1 sec
+Metro tic_tac(1000); // timer exécuté toutes les 1 sec
 DynamixelSerial AX12A_s;
-int AX_pince = 7;
-int AX_pot = 5;
 Servo servo1;
 Servo servo2;
 Servo servo3;
+bool up = false;
 void setup() 
 {
-    pinMode(STEPPER_2_DIR,OUTPUT);
-    pinMode(STEPPER_2_STP,OUTPUT);
-    digitalWrite(STEPPER_2_DIR,HIGH);
     pinMode(MAG1,OUTPUT);
     pinMode(MAG2,OUTPUT);
     pinMode(MAG3,OUTPUT);
@@ -53,24 +23,107 @@ void setup()
     // servo3.attach(SERVO3_PIN);
     
     Serial.begin(115200);
+    AX12A_s.init(&Serial3);
+    servo1.attach(SERVO1_PIN);
+    servo2.attach(SERVO2_PIN);
+    servo3.attach(SERVO3_PIN);
+    delay(500);
 
+    // init sequence  :
+
+    
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_UP,AX_SPEED);
+    AX12A_s.moveSpeed(AX_POT,POT_DOWN,AX_SPEED);
+    servo1.writeMicroseconds(SERVO1_CLOSED);
+    servo2.writeMicroseconds(SERVO2_CLOSED);
+    servo3.writeMicroseconds(SERVO3_CLOSED);
+    digitalWrite(MAG1,LOW);
+    digitalWrite(MAG2,LOW);
+    digitalWrite(MAG3,LOW);
+    
+    delay(3000);
+    
 }
 
 void loop() 
 {
-    if(tic_tac.check())
-    {
-        digitalToggle(LED_BUILTIN);
+
+    //test grab 1 plante
+    // attraper les plantes 1 puis 2 puis 3 
+    
+    //1
+    servo1.writeMicroseconds(SERVO1_OPEN);
+    delay(3000);
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_DOWN,AX_SPEED); // descend 
+    delay(1000);
+    servo1.writeMicroseconds(SERVO1_CLOSED); // attrape la plante
+    delay(500);
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_UP,AX_SPEED); // remonte
+    delay(2000);
+
+    //2 
+    servo2.writeMicroseconds(SERVO2_OPEN);
+    delay(3000);
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_DOWN,AX_SPEED); // descend 
+    delay(1000);
+    servo2.writeMicroseconds(SERVO2_CLOSED); // attrape la plante
+    delay(500);
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_UP,AX_SPEED); // remonte
+    delay(2000);
+    
+    //3
+    servo3.writeMicroseconds(SERVO3_OPEN);
+    delay(3000);
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_DOWN,AX_SPEED); // descend 
+    delay(1000);
+    servo3.writeMicroseconds(SERVO3_CLOSED); // attrape la plante
+    delay(500);
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_UP,AX_SPEED); // remonte
+    delay(2000);
+    
+
+    // pose dans les pots
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_MID,AX_SPEED); //decend les plantes dans les pots
+    delay(1000);
+    digitalWrite(MAG1,HIGH); //attrape le pot
+    digitalWrite(MAG2,HIGH); //attrape le pot
+    digitalWrite(MAG3,HIGH); //attrape le pot
+    servo1.writeMicroseconds(SERVO1_OPEN); // lache la plante
+    servo2.writeMicroseconds(SERVO2_OPEN); // lache la plante
+    servo3.writeMicroseconds(SERVO3_OPEN); // lache la plante
+    delay(1000);
+    AX12A_s.moveSpeed(AX_PINCE,PINCE_UP,AX_SPEED); // remonte
+    delay(1000);
+    servo1.writeMicroseconds(SERVO1_CLOSED); // referme la pince
+    servo2.writeMicroseconds(SERVO2_CLOSED); // referme la pince
+    servo3.writeMicroseconds(SERVO3_CLOSED); // referme la pince
+    delay(1000);
+
+    // pose les pots
+    AX12A_s.moveSpeed(AX_POT,POT_UP,AX_SPEED); // monte le pot
+    delay(2000);
+    digitalWrite(MAG1,LOW); //lache le pot
+    digitalWrite(MAG2,LOW); //lache le pot
+    digitalWrite(MAG3,LOW); //lache le pot
+    delay(3000);
+    AX12A_s.moveSpeed(AX_POT,POT_DOWN,AX_SPEED); // rentre le bras
+    delay(2000);
 
 
-        digitalToggle(MAG1);
-        digitalToggle(MAG2);
-        digitalToggle(MAG3);
+
+    // if(tic_tac.check())
+    // {
+    //     servo1.writeMicroseconds(up ? (int)SERVO1_OPEN : (int)SERVO1_CLOSED);
+    //     servo2.writeMicroseconds(up ? (int)SERVO2_OPEN : (int)SERVO2_CLOSED);
+    //     servo3.writeMicroseconds(up ? (int)SERVO3_OPEN : (int)SERVO3_CLOSED);
         
-    }
-    // servo1.writeMicroseconds(SERVO1_CLOSED);
-    // delay(1000);
-    // servo1.writeMicroseconds(SERVO1_OPEN);
-    // delay(1000);
+    //     digitalToggle(LED_BUILTIN);
+    //     up = !up;
+
+        
+    //     // digitalToggle(MAG2);
+    //     // digitalToggle(MAG3);
+        
+    // }
     
 }
