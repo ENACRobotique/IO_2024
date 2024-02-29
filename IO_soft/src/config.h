55f4a0fa48bd090d5e51236a115824301ff7f846
ENAC-Robotique <robotique.enac@gmail.com>
2024-02-29 15:14:11 +0100

    values correction

--- IO_soft/src/config.h
+++ IO_soft/src/config.h
@@ -7,21 +7,21 @@
 #define AX_POT 5
 #define AX_SPEED 300
 
-#define POT_UP 550
+#define POT_UP 540
 #define POT_DOWN 210
 
 #define PINCE_UP 975
 #define PINCE_MID 700
-#define PINCE_DOWN 270
+#define PINCE_DOWN 350
 
 #define SERVO1_PIN PA7
-#define SERVO1_OPEN 800
-#define SERVO1_CLOSED 1200
+#define SERVO1_OPEN 950
+#define SERVO1_CLOSED 1180
 
 #define SERVO2_PIN PA8
 #define SERVO2_OPEN 1600
 #define SERVO2_CLOSED 1950
 
 #define SERVO3_PIN PA9
-#define SERVO3_OPEN 1200
+#define SERVO3_OPEN 1350
 #define SERVO3_CLOSED 1516
