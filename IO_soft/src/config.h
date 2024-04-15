db7f99acd55977d31f0e1a84aa59c51e9d3f0e87
HugoLouisVitry <hugo.louis.vitry@gmail.com>
2024-04-16 01:40:43 +0200

    config nucleo

--- IO_soft/src/config.h
+++ IO_soft/src/config.h
@@ -1,27 +1,12 @@
-#define MAG1 PA11
-#define MAG3 PB8
-#define MAG2 PB9
+#define SERVO1 PB0
+#define SERVO2 PB6
+#define SERVO3 PA8
+#define SERVO4 PA6
+#define SERVO5 PA3
+#define SERVO6 PB7
 
-#define AX12A_PIN PC4
-#define AX_PINCE 7
-#define AX_POT 5
+#define AX_1_ID 
+#define AX_2_ID
 #define AX_SPEED 300
 
-#define POT_UP 540
-#define POT_DOWN 210
 
-#define PINCE_UP 975
-#define PINCE_MID 700
-#define PINCE_DOWN 350
-
-#define SERVO1_PIN PA7
-#define SERVO1_OPEN 950
-#define SERVO1_CLOSED 1180
-
-#define SERVO2_PIN PA8
-#define SERVO2_OPEN 1600
-#define SERVO2_CLOSED 1950
-
-#define SERVO3_PIN PA9
-#define SERVO3_OPEN 1350
-#define SERVO3_CLOSED 1516
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # IO_2024
-Carte et Soft pour les actionneurs de la coupe 2024
+Carte et Soft pour les actionneurs de la coupe 2024 ( recyclage de la carte servo testeur )
+
+Board : [Nucleo G431KB](https://www.st.com/en/evaluation-tools/nucleo-g431kb.html#documentation)
+Attention Pinout du servo 6 convention 5v | gnd | pwm  au lieu du gnd | 5v | pwm habituel !
